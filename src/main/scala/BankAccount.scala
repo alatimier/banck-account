33234caef327aad3f0ddfeb7cf5ca0bd9d8efa29
Arthur Latimier <arthur.latimier@gmail.com>
2020-10-14 23:26:16 +0200

    [US-1] Deposit

--- src/main/scala/BankAccount.scala
+++ src/main/scala/BankAccount.scala
@@ -1,13 +1,24 @@
-import java.util.Date
+import java.time.LocalDateTime
 
-case class Operation(date: Date, amount: Int, balance: Int)
+case class Operation(date: LocalDateTime, amount: Int, balance: Int)
 
-class BankAccount {
+class BankAccount(operations: Seq[Operation]) {
 
-  def deposit(amount: Int): Unit = ???
+  def deposit(amount: Int): BankAccount = {
+    if (amount < 0) {
+      throw new IllegalArgumentException()
+    }
+    new BankAccount(operations :+ Operation(LocalDateTime.now(), amount, getBalance() + amount))
+  }
 
-  def withdraw(amount: Int): Unit = ???
+  def withdraw(amount: Int): BankAccount = ???
 
-  def printStatement(): Seq[Operation] = ???
+  def getBalance(): Int = if (operations.isEmpty) 0 else operations.last.balance
 
-} 
+  def printStatement(): List[Operation] = ???
+
+}
+
+object BankAccount {
+  def apply(): BankAccount = new BankAccount(List())
+}
--- src/test/scala/BankAccountTest.scala
+++ src/test/scala/BankAccountTest.scala
@@ -1,5 +1,28 @@
 class BankAccountTest extends org.scalatest.funsuite.AnyFunSuite {
 
+  test("Should not depose negative amount") {
+    assertThrows[IllegalArgumentException] {
+      BankAccount().deposit(-1)
+    }
+  }
 
+  test("Should be empty at creation") {
+    // When
+    val bankAccount = BankAccount()
+
+    // Then
+    assert(bankAccount.getBalance() == 0)
+  }
+
+  test("Should update balance after deposits") {
+    // When
+    val bankAccount = BankAccount()
+      .deposit(100)
+      .deposit(200)
+      .deposit(50)
+
+    // Then
+    assert(bankAccount.getBalance() == 350)
+  }
 
 }
